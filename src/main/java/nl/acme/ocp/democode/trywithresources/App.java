05d82e6e42191ecf0830efdd1344ecca94842a8b
Raymond Loman <raymond.loman@edsn.nl>
2026-03-24 17:27:12 +0100

    Fix a warning

--- src/main/java/nl/acme/ocp/democode/trywithresources/App.java
+++ src/main/java/nl/acme/ocp/democode/trywithresources/App.java
@@ -3,7 +3,7 @@ package nl.acme.ocp.democode.trywithresources;
 public class App {
 
     public static void main(String[] args) {
-        try(Box<Integer> b = new Box()) {
+        try(Box<Integer> b = new Box<>()) {
             System.out.println(b);
             b.add(3);
         }
