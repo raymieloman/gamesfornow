90402b9549c6cee201043decfbf0fc043d8780b0
Raymond Loman <raymond@carpago.nl>
2026-03-29 17:13:37 +0200

    Make hanoi static

--- src/main/java/nl/acme/ocp/democode/recursion/hanoi/App.java
+++ src/main/java/nl/acme/ocp/democode/recursion/hanoi/App.java
@@ -1,9 +1,9 @@
 package nl.acme.ocp.democode.recursion.hanoi;
 
-public class App {
+import static nl.acme.ocp.democode.recursion.hanoi.Hanoi.hanoi;
 
+public class App {
     public static void main(String[] args) {
-        Hanoi h = new Hanoi();
-        h.hanoi(3);
+        hanoi(3);
     }
 }
